a1d58023127496f2e804944885473646d2dcc41a
Jon Loftus <83649313+med1ocre@users.noreply.github.com>
2023-07-26 10:43:42 -0400

    Woodcutting

--- src/js/data.js
+++ src/js/data.js
@@ -18,5 +18,104 @@ let player = {
 }
 
 
+let Skill = {
+  Woodcutting: {
+    Level: 1,
+    currentXP: 0,
+    xpToLevel: 50,
+    currentMastery: 1,
+    totalMastery: 7,
+    Trees: {
+      Oak: {
+        xpReward: 10,
+        chopTime: 3000,
+        itemReward: {
+          name: "Oak Log",
+          item: "oakLog",
+        },
+        masteryXP: 0,
+        masteryLevel: 1
+      },
+      Pine: {
+        xpReward: 15,
+        chopTime: 4000,
+        itemReward: {
+          name: "Pine Log",
+          item: "pineLog",
+        },
+        masteryXP: 0,
+        masteryLevel: 1
+      },
+      Willow: {
+        xpReward: 25,
+        chopTime: 5000,
+        itemReward: {
+          name: "Willow Log",
+          item: "willowLog",
+        },
+        masteryXP: 0,
+        masteryLevel: 1
+      },
+      Maple: {
+        xpReward: 40,
+        chopTime: 6000,
+        itemReward: {
+          name: "Maple Log",
+          item: "mapleLog",
+        },
+        masteryXP: 0,
+        masteryLevel: 1
+      },
+      Yew: {
+        xpReward: 75,
+        chopTime: 8000,
+        itemReward: {
+          name: "Yew Log",
+          item: "yewLog",
+        },
+        masteryXP: 0,
+        masteryLevel: 1
+      },
+      Magic: {
+        xpReward: 100,
+        chopTime: 10000,
+        itemReward: {
+          name: "Magic Log",
+          item: "magicLog",
+        },
+        masteryXP: 0,
+        masteryLevel: 0
+      },
+      Redwood: {
+        xpReward: 150,
+        chopTime: 12000,
+        itemReward: {
+          name: "Redwood Log",
+          item: "redwoodLog",
+        },
+        masteryXP: 0,
+        masteryLevel: 1
+      }
+    }
+  }
+};
+
+const masteryRequiredXP = [
+  0, 83, 174, 276, 388, 512, 650, 801, 969, 1154, 1358, 1584, 1833, 2107, 2411, 2746,
+  3115, 3523, 3973, 4470, 5018, 5624, 6291, 7028, 7842, 8740, 9730, 10824, 12031,
+  13363, 14833, 16456, 18247, 20224, 22406, 24815, 27473, 30408, 33648, 37224, 41171,
+  45529, 50339, 55649, 61512, 67983, 75127, 83014, 91721, 101333, 111945, 123660, 136594,
+  150872, 166636, 184040, 203254, 224466, 247886, 273742, 302288, 333804, 368599, 407015,
+  449428, 496254, 547953, 605032, 668051, 737627, 814445, 899257, 992895, 1096278, 1210421,
+  1336443, 1475581, 1629200, 1798808, 1986068, 2192818, 2421087, 2673114, 2951373, 3258594,
+  3597792, 3972294, 4385776, 4842295, 5346332, 5902831, 6517253, 7195629, 7944614, 8771558,
+  9684577, 10692629, 11805606, 13034431, 14391160
+];
+
+
+let oakTreeCurrentXPDisplay = document.getElementById("oak-mastery-xp");
+let oakTreeRequiredXPDisplay = document.getElementById("oak-mastery-required-xp");
+
+
 //This is our temp var we use as a "skeleton" when exchanging item info
 let Item;
--- src/js/script.js
+++ src/js/script.js
@@ -1,35 +1,94 @@
-function startCutting(treeElement, xpReward) {
+function calcReqMasteryXP(treeElement){
   const progressBar = treeElement.querySelector(".progress-bar");
-  const oakDuration = 3000; // 3 seconds for an oak tree
-  const pineDuration = 4000; // 4 seconds for a pine tree
-  const totalTime = treeElement.dataset.treeType === "oak" ? oakDuration : treeElement.dataset.treeType === "pine" ? pineDuration : 0;
-  const increment = totalTime / 100; // Adjust this value for smoothness
+  const attr = progressBar.getAttribute('data-xp-reward');
+  const tree = Skill.Woodcutting.Trees[attr];
 
-  let startTime;
+  const level = tree.masteryLevel;
+  const requiredXP = masteryRequiredXP[level];
 
-  function updateProgressBar(timestamp) {
-    if (!startTime) startTime = timestamp;
-    const currentTime = timestamp - startTime;
-    const progress = (currentTime / totalTime) * 100;
-    progressBar.style.width = progress + "%";
+  return requiredXP;
 
-    if (currentTime < totalTime) {
-      requestAnimationFrame(updateProgressBar);
-    } else {
-      // Ensure the progress bar reaches 100%
-      progressBar.style.width = "100%";
-      // Give XP reward
-      // You can add your XP reward logic here. For now, I'll just show an alert
-      console.log("XP GAINED!");
 
-      // Restart cutting action
-      setTimeout(() => {
-        progressBar.style.width = "0%";
-        startTime = null; // Reset startTime to null for the new cutting cycle
-        requestAnimationFrame(updateProgressBar);
-      }, 1000); // Add a delay before restarting the cutting action (1 second in this example)
-    }
-  }
+}
 
-  requestAnimationFrame(updateProgressBar);
+function calcMasteryXPRewardTree(treeElement){
+
+  const progressBar = treeElement.querySelector(".progress-bar");
+  const attr = progressBar.getAttribute('data-xp-reward');
+  const tree = Skill.Woodcutting.Trees[attr];
+
+  const trees = Skill.Woodcutting.Trees;
+
+  const totalItems = Object.keys(trees).length;
+  const itemMasteryLevel = tree.masteryLevel;
+  const playerMastery = Skill.Woodcutting.currentMastery;
+  const totalMastery = Object.keys(trees).length * 99;
+  const chopTime = tree.chopTime / 1000;
+
+  const v1 = playerMastery / totalMastery;
+  const v2 = totalItems / 10;
+  const v3 = itemMasteryLevel * v2;
+  const v4 = v1 + v3;
+  const v5 = v4 * chopTime;
+  const v6 = v5 * 0.5;
+  const v7 = v6 * 1; //Add multiplier here
+
+  return(v7);
+
+}
+
+
+
+function startCutting(treeElement) {
+  const progressBar = treeElement.querySelector(".progress-bar");
+  const attr = progressBar.getAttribute('data-xp-reward');
+  const tree = Skill.Woodcutting.Trees[attr];
+
+  const chopTime = tree.chopTime;
+
+  progressBar.style.transition = `width ${chopTime}ms linear`;
+  progressBar.style.width = "100%";
+
+  setTimeout(() => {
+
+    finishCut(treeElement, attr, tree)
+
+}, chopTime);
+
+  
+
+
+}
+
+function finishCut(treeElement, attr, tree){
+
+  //Set level text and other stuff
+  const progressBar = treeElement.querySelector(".progress-bar");
+  const masteryRequiredXP = treeElement.querySelector("#mastery-required-xp");
+  const masteryCurrentXP = treeElement.querySelector("#mastery-xp");
+  const masteryLevel = treeElement.querySelector("#mastery-xp");
+
+
+  const newItem = tree.itemReward.item;
+  const itemArray = Skill.Woodcutting.totalItems;
+
+
+
+  Skill.Woodcutting.currentXP += tree.xpReward;
+  tree.masteryXP += calcMasteryXPRewardTree(treeElement);
+
+  masteryRequiredXP.innerHTML = calcReqMasteryXP(treeElement);
+  masteryCurrentXP.innerHTML = Math.floor(tree.masteryXP);
+
+  progressBar.style.transition = 'none';
+  progressBar.style.width = "0%";
+
+
+  setTimeout(() => {
+
+      startCutting(treeElement);
+
+  }, 20);
+  
+
 }
